aff06d3204a97e62dce2ab424ba7bc84dcbca6c9
Sahil <sahilkhemariya327@gmail.com>
2025-03-03 12:54:44 +0530

    [Sahil] Add. Unit conversion

--- sequence_problems/SumAverage.js
+++ sequence_problems/SumAverage.js
@@ -1,15 +1,15 @@
 // Take 5 numbers to sum and average
 
-let sum = 0;
-let average = 0;
+let sum = 0; // To store sum
+let average = 0; // To store average        
 
 for(let i=0; i<5; i++) {
-    let a = Math.floor(Math.random()*90)+10;
+    let a = Math.floor(Math.random()*90)+10; // To get the number in each iteration
     console.log("number ",i+1," is ",a);
-    sum += a;
+    sum += a; // update total sum
 }
 
-average = sum/5;
+average = sum/5; // Calculate average
 
 console.log("Sum of number: ",sum);
 console.log("average of number: ",average);
